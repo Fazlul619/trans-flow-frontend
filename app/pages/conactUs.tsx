8376e1b5e892bc2d80100369d98e9761b51be0a6
Fazlul Alam <fazlulalam619@gmail.com>
2025-03-22 01:01:55 +0600

    design

--- app/pages/conactUs.tsx
+++ app/pages/conactUs.tsx
@@ -2,9 +2,27 @@ import React from "react";
 
 export function ContactUs (){
     return (
-        <div>
-            <h1>Contact Us</h1>
-            <p>Please fill out the form below to get in touch.</p>
+    <div className="max-w-5xl">
+      <div className="absolute w-full h-screen ">
+        <img className="w-full h-screen " src="/public/images/Contact.png" alt="" />
+        <div className="relative -top-[600px] left-[200px] w-[487px]">
+        <form className="">
+            <div>
+              <label className="block text-[#345485]">Full Name</label>
+              <input type="text" placeholder="Your full name" className="w-full bg-white mt-2 p-2 h-14  rounded-lg text-black" />
+            </div>
+            <div className="mt-4">
+              <label className="block text-[#345485]">Email</label>
+              <input type="email" placeholder="example@gmail.com" className="w-full bg-white h-14 text-black mt-2 p-2  rounded-lg" />
+            </div>
+            <div className="mt-4">
+              <label className="block text-[#345485]">Message</label>
+              <textarea placeholder="Write message" className="w-full mt-2 p-2 h-[140px] text-black bg-white rounded-lg" rows={4}></textarea>
+            </div>
+            <button className="w-full bg-[#345485] text-white mt-4 p-2 rounded-lg hover:bg-blue-800">Submit</button>
+        </form>
         </div>
+      </div>
+    </div>
     );
 }
