13064d082b6f91eb653a846c8276050b9cf3a502
Fazlul Alam <fazlulalam619@gmail.com>
2025-03-21 23:57:24 +0600

    project setup

---
+++ app/pages/conactUs.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function ContactUs (){
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>Please fill out the form below to get in touch.</p>
+        </div>
+    );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,9 @@
+import { ContactUs } from "~/pages/conactUs";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return < ContactUs/>;
 }
